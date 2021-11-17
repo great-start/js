0ebfebdf0db5c40ee699f534dfb713a07a3886ef
ev <zhenya.bill@gmail.com>
2021-11-17 12:55:23 +0200

    14.11_hw4

--- lesson3/cw3/cw3_txt/cw3_txt.js
+++ lesson3/cw3/cw3_txt/cw3_txt.js
@@ -12,7 +12,7 @@
 
 let arr = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
 i = 0;
-while (i < arr.length) {                       // 1.
+while (i < arr.length) {                    // 1.
     console.log(arr[i]);
     i++;
 }
