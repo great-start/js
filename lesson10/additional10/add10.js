e5c59f33e15176e13e6c012b90a243e3f9097d7d
ev <zhenya.bill@gmail.com>
2021-12-08 23:10:07 +0200

    8.12.21 add10_done

--- lesson10/additional10/add10.js
+++ lesson10/additional10/add10.js
@@ -1,3 +1,4 @@
+
 // - Описати скріпт, котрий, якщо доєднати до будь-якої сторінки дозволить зробити наступне:
 //     При лівому кліку миші вивести в консоль інформацію про блок або елемент на який відбувся клік.
 //     Інформація яку потрібно вивести: Назва тегу, список класів, список ід, розміри в форматі висота*ширина
@@ -10,43 +11,43 @@ document.body.addEventListener('click', function (e) {
     info.height_width = `${e.target.clientHeight} * ${e.target.clientWidth}`;
     console.log(info);
 })
-
-
-
-//---------------------------------------------------------------------------------------------------------------------------------------------------
-// - Описати скріпт, котрий, якщо доєднати до будь-якої сторінки дозволить зробити наступне:
-//     При лівому кліку миші  зробить popup (спливаючий блок) в якому буде вся інформація про блок.
-//     Інформація яку потрібно вивести в popup: Назва тегу, список класів, список ід, розміри в форматі висота*ширина
-
-// document.body.addEventListener('mousedown', function (e) {
-//     const divPopup = document.createElement('div');
-//     divPopup.setAttribute('id', 'info');
-//     divPopup.style.position = 'absolute';
-//     divPopup.style.top = e.clientY + 'px';
-//     divPopup.style.left = e.clientX + 'px';
-//     divPopup.style.width = '200px';
-//     divPopup.style.height = '200px';
-//     divPopup.style.background = 'lightcyan';
-//     divPopup.style.opacity = '0.9';
-//     const pTag = document.createElement('p');
-//     pTag.innerHTML = `<b>Тег</b>: ${e.target.tagName}`;
-//     const pClasses = document.createElement('p');
-//     pClasses.innerHTML = `<b>Класси</b>: ${e.target.classList}`;
-//     const pId = document.createElement('p');
-//     pId.innerHTML = `<b>Id</b>: ${e.target.getAttribute('id')}`;
-//     const pHeight_Width = document.createElement('p');
-//     pHeight_Width.innerHTML = `<b>Height*Width</b>: ${e.target.clientHeight}px*${e.target.clientWidth}px`;
 //
-//     document.body.append(divPopup);
-//     divPopup.append(pTag, pClasses, pId, pHeight_Width);
-// });
 //
-// document.body.addEventListener('mouseup', function () {
-//     const elementById = document.getElementById('info');
-//     elementById.remove();
-// });
 //
-// document.write('<hr>');
+// //---------------------------------------------------------------------------------------------------------------------------------------------------
+// // - Описати скріпт, котрий, якщо доєднати до будь-якої сторінки дозволить зробити наступне:
+// //     При лівому кліку миші  зробить popup (спливаючий блок) в якому буде вся інформація про блок.
+// //     Інформація яку потрібно вивести в popup: Назва тегу, список класів, список ід, розміри в форматі висота*ширина
+//
+document.body.addEventListener('mousedown', function (e) {
+    const divPopup = document.createElement('div');
+    divPopup.setAttribute('id', 'info');
+    divPopup.style.position = 'absolute';
+    divPopup.style.top = e.clientY + 'px';
+    divPopup.style.left = e.clientX + 'px';
+    divPopup.style.width = '200px';
+    divPopup.style.height = '200px';
+    divPopup.style.background = 'lightcyan';
+    divPopup.style.opacity = '0.9';
+    const pTag = document.createElement('p');
+    pTag.innerHTML = `<b>Тег</b>: ${e.target.tagName}`;
+    const pClasses = document.createElement('p');
+    pClasses.innerHTML = `<b>Класси</b>: ${e.target.classList}`;
+    const pId = document.createElement('p');
+    pId.innerHTML = `<b>Id</b>: ${e.target.getAttribute('id')}`;
+    const pHeight_Width = document.createElement('p');
+    pHeight_Width.innerHTML = `<b>Height*Width</b>: ${e.target.clientHeight}px*${e.target.clientWidth}px`;
+
+    document.body.append(divPopup);
+    divPopup.append(pTag, pClasses, pId, pHeight_Width);
+});
+
+document.body.addEventListener('mouseup', function () {
+    const elementById = document.getElementById('info');
+    elementById.remove();
+});
+
+document.write('<hr>');
 
 
 
@@ -166,14 +167,106 @@ divCheckboxes.addEventListener('click', function () {
 
 
 
-
 //---------------------------------------------------------------------------------------------------------------------------------------------------
 // *****(Прям овердоз с рекурсией) Создать функцию которая принимает какой-либо элемент DOM-структуры .Функция создает в боди 2 кнопки (назад/вперед)
 // при нажатии вперед, вы переходите к дочернему элементу, при еще одном нажатии на "вперед", вы переходите к следующему дочернему элементу (лежащему на одном уровне)
 // НО если у (какого-либо)дочеренего элемента есть дети, то нажатие "вперед" позволяет нам войти внутрь элемента и  выводит первого ребенка. и тд.
 //     Когда все дети заканчиваются, мы выходим из данного дочернего элемента и переходим к следующему, лежащему с ним на одном уровне
-//
+function transition(element) {
+    const divSelector = document.createElement('div');
+    divSelector.classList.add('selector');
+    const up = document.createElement('button');
+    up.innerText = 'Вперед';
+    const down = document.createElement('button');
+    down.innerText = 'Назад';
+
+    document.body.append(divSelector);
+    divSelector.append(up, down);
+
+    const elementsObj = {};
+    let index = 0;
+
+    function recursive(element) {
+        if (element.children.length) {
+            for (const elementElement of element.children) {
+                elementsObj[index] = elementElement;
+                index++;
+                recursive(elementElement);
+            }
+        }
+    }
+
+    recursive(element);
+
+    let click = 0;
+    up.onclick = () => {
+        if (elementsObj[click]) {
+            console.log(elementsObj[click]);
+            click++;
+        }
+    };
+
+    down.onclick = () => {
+        click--;
+        if (elementsObj[click]) {
+            console.log(elementsObj[click]);
+        }
+    };
+}
+
+transition(document.body);
+
+
+
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------
 // - Напишите «Карусель» – ленту изображений, которую можно листать влево-вправо нажатием на стрелочки.
-//
+const carouselDiv = document.createElement('div');
+carouselDiv.classList.add('carousel');
+carouselDiv.style.backgroundImage = 'url("img/1.jpg")';
+const left = document.createElement('button');
+left.innerText = 'Left';
+const right = document.createElement('button');
+right.innerText = 'Right';
+
+document.body.append(carouselDiv);
+carouselDiv.append(left, right);
+
+let point = 1;
+right.onclick = () => {
+    point >= 4 ? point = 0 : false;
+    point++;
+    carouselDiv.style.backgroundImage = '' + `url("img/${point}.jpg")` + '';
+    console.log(point);
+    point >= 4 ? point = 0 : false;
+};
+
+left.onclick = () => {
+    point--;
+    point <= 0 ? point = 4 : false;
+    carouselDiv.style.backgroundImage = '' + `url("img/${point}.jpg")` + '';
+    console.log(point);
+
+};
+
+
+
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
 //     Завдання важке для розуміння, але дуже легке в реалізації. Тут треба буде погуглити
 // *** При виділені сегменту тексту на сторінці він стає жирний/курсивний/або якось іншим способом змінює свій стан
+
+
+document.body.onmousedown = () => document.getSelection().removeAllRanges();
+
+document.body.onclick = function (e) {
+    let selObj = document.getSelection();
+    e.target.innerHTML = e.target.innerText.slice(0, selObj.anchorOffset) + '<span style="color: red">' + e.target.innerText.slice(selObj.anchorOffset, selObj.focusOffset) +
+        '</span>' + e.target.innerText.slice(selObj.focusOffset, e.target.innerText.length);
+
+}
+
+
+
+
