cc2230a140eb426797253f8ccc25ac8b1fb32cab
ev <zhenya.bill@gmail.com>
2021-12-16 14:37:50 +0200

    12.12.21 add13_final_version

--- lesson13/additional13/list.js
+++ lesson13/additional13/list.js
@@ -14,25 +14,27 @@ productsWrap.classList.add('productsWrap');
 let products = JSON.parse(localStorage.getItem('products'));
 
 if (products) {
-    products.forEach(product => {
-        let productBox = document.createElement('div');
-        productBox.innerHTML = `<h3>${product.name}</h3>
-                                <h4>Кількість: ${product.amount} шт.</h4>
-                                <h4>Ціна одиниці: ${product.price} грн.</h4>
-                                <img src="${product.url}" alt="product photo"> `;
-        let deleteProduct = document.createElement('button');
-        deleteProduct.classList.add('delProd');
-        deleteProduct.innerText = 'Видалити товар';
+    products.forEach((product, index) => {
+        setTimeout(() => {                          // - добавил периодичность появления списка - для красоты
+            let productBox = document.createElement('div');
+            productBox.innerHTML = `<h3>${product.name}</h3>
+                                    <h4>Кількість: ${product.amount} шт.</h4>
+                                    <h4>Ціна одиниці: ${product.price} грн.</h4>
+                                    <img src="${product.url}" alt="product photo"> `;
+            let deleteProduct = document.createElement('button');
+            deleteProduct.classList.add('delProd');
+            deleteProduct.innerText = 'Видалити товар';
 
-        deleteProduct.onclick = () => {
-            products.splice(products.indexOf(product), 1);
-            localStorage.setItem('products', JSON.stringify(products));
-            document.location.reload();
-        }
-
-        productBox.append(deleteProduct);
-        productsWrap.append(productBox);
-        document.body.append(productsWrap);
+            deleteProduct.onclick = () => {
+                products.splice(products.indexOf(product), 1);
+                localStorage.setItem('products', JSON.stringify(products));
+                document.location.reload();
+            }
+            productBox.append(deleteProduct);
+            productsWrap.append(productBox);
+            document.body.append(productsWrap);
+            console.log(product);
+        }, index * 500);
     });
 } else {
     document.write('У списку немає жодного товару');
