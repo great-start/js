3f083e741c0ebdc31a2b240b3d11110275498ba4
ev <zhenya.bill@gmail.com>
2021-12-19 20:24:42 +0200

    19.12.21 ev_loop

--- lesson14/index14.js
+++ lesson14/index14.js
@@ -52,14 +52,15 @@ setTimeout(function wakeUp(ifAlive) {
 //--------------------------------------------------------------------------------------------------------------------------------------------------
 function wakeUp(ifAlive) {
     return new Promise((resolve, reject) => {
-        if (ifAlive) {
-            let morningEvents = {
-                wakeUp: true,
-            };
-            console.log(morningEvents);
-            resolve(morningEvents);
-        }
-        reject('I can`t wake up. Something happen');
+        setTimeout(() => {
+            if (ifAlive) {
+                let morningEvents = {
+                    wakeUp: true,
+                };
+                resolve(morningEvents);
+            }
+            reject('I can`t wake up. Something happen');
+        }, 6000);
     });
 }
 
@@ -71,38 +72,44 @@ function morningWash(morningEvents) {
                 resolve(morningEvents);
             }
             reject('I can`t do morning washing');
-        }, 8000);
+        }, 1000);
     })
 }
 
 
 function doMorningExercise(morningEvents) {
     return new Promise((resolve, reject) => {
-        if (morningEvents.morningWash && morningEvents.wakeUp) {
-            morningEvents.doMorningExercise = true;
-            resolve(morningEvents);
-        }
-        reject('didn`t do morning exercises');
+        setTimeout(() => {
+            if (morningEvents.morningWash && morningEvents.wakeUp) {
+                morningEvents.doMorningExercise = true;
+                resolve(morningEvents);
+            }
+            reject('didn`t do morning exercises');
+        }, 1000);
     })
 }
 
 function haveBreakfast(morningEvents) {
     return new Promise((resolve, reject) => {
-        if (morningEvents.wakeUp && morningEvents.morningWash && morningEvents.doMorningExercise) {
-            morningEvents.haveBreackfast = true;
-            resolve(morningEvents);
-        }
-        reject('Couldn`t eat');
+        setTimeout(() => {
+            if (morningEvents.wakeUp && morningEvents.morningWash && morningEvents.doMorningExercise) {
+                morningEvents.haveBreackfast = true;
+                resolve(morningEvents);
+            }
+            reject('Couldn`t eat');
+        }, 1000);
     })
 }
 
 function goToWork(morningEvents) {
     return new Promise((resolve, reject) => {
-        if (morningEvents.haveBreackfast) {
-            morningEvents.goToWork = true;
-            resolve(morningEvents);
-        }
-        reject('It is hard for me to go working :)');
+        setTimeout(() => {
+            if (morningEvents.haveBreackfast) {
+                morningEvents.goToWork = true;
+                resolve(morningEvents);
+            }
+            reject('It is hard for me to go working :)');
+        }, 1000);
     })
 }
 
@@ -128,17 +135,22 @@ wakeUp(true)
 async function morningSchedule() {
     try {
         let wakeUp1 = await wakeUp(true);
+        console.log(wakeUp1);
         let morningWashing1 = await morningWash(wakeUp1);
+        console.log(morningWashing1);
         let doMorningExercise1 = await doMorningExercise(morningWashing1);
+        console.log(doMorningExercise1);
         let haveBreakfast1 = await haveBreakfast(doMorningExercise1);
-        return goToWork(haveBreakfast1).then(morningEvents => {
-                for (const eventKey in morningEvents) {
-                    console.log(`${eventKey}: ${morningEvents[eventKey]}`);
-                }
-            })
+        console.log(haveBreakfast1);
+        return goToWork(haveBreakfast1);
     } catch (e) {
         console.log(e);
     }
 }
 
-morningSchedule();
+morningSchedule()
+    .then(morningEvents => {
+    for (const eventKey in morningEvents) {
+        console.log(`${eventKey}: ${morningEvents[eventKey]}`);
+    }
+});
